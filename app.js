3fcb46c80cb3af224f985cdf03933e5571978fec
DBuckley0126 <DannyBoy.0126>
2020-04-10 17:18:34 +0100

    Add ejs views

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const Koa = require('koa');
-const app = new Koa();
+const KoaRouter = require('koa-router')
 const json = require('koa-json')
+const path = require('path')
+const render = require('koa-ejs') 
 
+const app = new Koa();
+const router = new KoaRouter();
+
+// Json Prettier Middleware
 app.use(json())
 
-app.use(async ctx => (ctx.body = {msg: 'Hello world'}));
+
+// app.use(async ctx => (ctx.body = {msg: 'Hello world'}));
+router.get('/test', ctx => (ctx.body = {msg: 'Hello world'}))
+
+render(app, {
+  root:path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cashe: false,
+  debug: true
+})
+
+// Index
+router.get('/', async ctx => {
+  await ctx.render('index')
+})
+
+
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
+
 
 app.listen(3000, () => console.log('Server Starting...'))
