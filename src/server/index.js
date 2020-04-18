6afe3ddb32adc7c313f02a8479286641dd70f3c0
DBuckley0126 <DannyBoy.0126>
2020-04-18 16:09:42 +0100

    Add Koa logger middlware

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const Koa = require("koa");
+const logger = require('koa-logger')
 const KoaBody = require("koa-body");
 const Json = require("koa-json");
 
@@ -8,10 +9,12 @@ const movieRoutes = require("./routes/movies");
 const app = new Koa();
 const PORT = process.env.PORT || 5000;
 
-// Json Prettier Middleware
+// JSON Prettier middleware
 app.use(Json());
 // Add koa-body middleware
 app.use(KoaBody());
+// Add koa-logger middleware
+app.use(logger())
 
 app.use(indexRoutes.routes());
 app.use(movieRoutes.routes());
