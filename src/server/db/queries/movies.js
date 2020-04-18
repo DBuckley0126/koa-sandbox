874fc53d647d0edee7f9dc0ad65ab5971becb6d1
DBuckley0126 <DannyBoy.0126>
2020-04-18 15:35:17 +0100

    Add DELETE route with tests

--- src/server/db/queries/movies.js
+++ src/server/db/queries/movies.js
@@ -23,8 +23,17 @@ function updateMovie(id, movie) {
   .returning('*');
 }
 
+function deleteMovie(id) {
+  return knex('movies')
+  .del()
+  .where({ id: parseInt(id) })
+  .returning('*');
+}
+
 module.exports = {
   getAllMovies,
   getSingleMovie,
-  addMovie
+  addMovie,
+  updateMovie,
+  deleteMovie
 };
